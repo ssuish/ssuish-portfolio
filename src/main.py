220fa49a3d1f43719db7f479aa12d4a902456ad2
ssuish <quidor.adrean@gmail.com>
2026-03-06 11:18:47 +0800

    Implemented file copying from static to public directory. Uses test css and img

--- src/main.py
+++ src/main.py
@@ -1,10 +1,36 @@
+import os
+import shutil
 from textnode import TextNode, TextType
 
+
+def copy_files(source_dir, dest_dir):
+    for i in os.listdir(source_dir):
+        source = os.path.join(source_dir, i)
+        dest = os.path.join(dest_dir, i)
+
+        print(f"Copying {source} to {dest}")
+
+        if os.path.isfile(source):
+            shutil.copy(source, dest)
+        else:
+            os.mkdir(dest)
+            copy_files(source, dest)
+
+
+def static_to_public():
+    public_dir_abs = os.path.abspath("./public")
+    static_dir_abs = os.path.abspath("./static")
+
+    if os.path.exists(public_dir_abs):
+        shutil.rmtree(public_dir_abs)
+    os.makedirs(public_dir_abs, exist_ok=True)
+
+    copy_files(static_dir_abs, public_dir_abs)
+
+
 def main():
-    text = TextNode(
-        "This is some anchor text", TextType.LINK, "https://example.com"
-    )
-    print(text)
+    static_to_public()
+
 
 if __name__ == "__main__":
     main()
